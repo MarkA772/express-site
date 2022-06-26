a61e0e0a941dc84094b55c90d75fc69e19f0c12e
Mark Alexander <markalexander@example.com>
2022-06-25 19:15:32 -0700

    Added db routing to express site

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const gameRouter = require('./routes/game');
+const dbRouter = require('../turtle-algo-game/server/routes/dbRouter');
 
 const app = express();
 const port = 80;
@@ -10,6 +11,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/games', gameRouter);
+app.use('/api', dbRouter);
 
 app.use((req, res, next) => {
   res.send('<h1>Sorry, not found.</h1>');
@@ -18,5 +20,6 @@ app.use((req, res, next) => {
 app.listen(port, () => {
   console.log('Express Site listening on port ' + port);
 });
+
 
 module.exports = app;
